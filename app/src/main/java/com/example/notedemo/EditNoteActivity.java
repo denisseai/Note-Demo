ed3802c752418b4958a9a2b7485f7d1f14a8cc92
dai <msdenisseanaya@gmail.com>
2019-03-29 14:26:32 -0700

    Note app demo with "add new note" in menu, long click to delete

---
+++ app/src/main/java/com/example/notedemo/EditNoteActivity.java
@@ -0,0 +1,54 @@
+package com.example.notedemo;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+import java.util.HashSet;
+
+public class EditNoteActivity extends AppCompatActivity {
+    int noteId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_note);
+
+        EditText editText = findViewById(R.id.editText);
+
+        Intent intent = getIntent();
+        noteId = intent.getIntExtra("noteId", -1);
+
+        if(noteId != -1){
+            editText.setText(MainActivity.notes.get(noteId));
+        }else{
+            MainActivity.notes.add("");
+            noteId = MainActivity.notes.size() -1;
+        }
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                MainActivity.notes.set(noteId, String.valueOf(s));
+                MainActivity.arrayAdapter.notifyDataSetChanged();
+
+                SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.example.notedemo", Context.MODE_PRIVATE);
+                HashSet<String> set = new HashSet<>(MainActivity.notes);
+                sharedPreferences.edit().putStringSet("notes", set).apply();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+}
